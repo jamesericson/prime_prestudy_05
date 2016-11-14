5098b40ca5e1b413fe3f6255007b3a6099c1c78b
James Ericson <jamesericson@gmail.com>
2016-11-13 23:40:00 -0600

    corrected syntax for object

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hello!");
+
+var avengersMovie = {
+    name : "Avengers", // Quotes added : semi-colon (and following others) changed to camma
+    runTime : 143, // removed "min" bc it's an integer
+    releaseYear : 2012, // changed equal sign to colon
+    director : "Joss Whedon" //removed quotes : removed capital 'D': removed period
+};//added a semi-colon
+
+console.log("Below is a log out of the \"avengersMovie\" object");
+console.log(avengersMovie);
